3caa53e3bfffd99761aade1a4f1a7918f380ad9b
Ziad H <ziadbc@gmail.com>
2023-07-09 01:27:35 -0700

    settings page

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-let timer = 25 * 60; // Pomodoro timer is traditionally 25 minutes
+let duration = 25; // Default Pomodoro duration
+let timer = duration * 60;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('view engine', 'ejs');
 
 function formatTime(seconds) {
@@ -15,6 +17,16 @@ function formatTime(seconds) {
 
 app.get('/', (req, res) => {
     res.render('index', { timer: formatTime(timer) });
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings', { duration: duration });
+});
+
+app.post('/settings', (req, res) => {
+    duration = Number(req.body.duration);
+    timer = duration * 60;
+    res.redirect('/');
 });
 
 io.on('connection', (socket) => {
@@ -29,7 +41,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('reset_timer', () => {
-        timer = 25 * 60;
+        timer = duration * 60;
         io.emit('timer', formatTime(timer));
     });
 });
