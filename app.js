c6e6577d3d652a0002301bfff543dd7aacf7edd1
Ziad H <ziadbc@gmail.com>
2023-10-11 17:53:18 -0700

    Change port

--- app.js
+++ app.js
@@ -88,6 +88,6 @@ io.on('connection', (socket) => {
 	
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(3838, () => {
+    console.log('listening on *:3838');
 });
